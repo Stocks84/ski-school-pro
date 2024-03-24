4152412a37462a283580d7f778d9a2af466a5d68
William Stocks <willstocks@me.com>
2024-03-24 09:55:34 +0000

    Add class to forms.py registration form

--- ski_school/booking/forms.py
+++ ski_school/booking/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Booking
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['lesson', 'student']  # Adjust fields as needed
+        fields = ['lesson', 'student']  
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
