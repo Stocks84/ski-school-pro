6760119905596c6ec6892067cae4d8edd16ae4e3
William Stocks <willstocks@me.com>
2024-03-24 16:41:22 +0000

    Add bootstrap & some CSS style to base html

---
+++ ski_school/booking/static/booking/js/script.js
@@ -0,0 +1,7 @@
+// your_script.js
+
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code here
+    console.log('Your JavaScript file is loaded!');
+});
