c49fee4ee7f5607cee43605a53452aeef399223f
William Stocks <willstocks@me.com>
2024-03-24 10:20:59 +0000

    Add login_form.html & the url path in urls.py

--- ski_school/booking/urls.py
+++ ski_school/booking/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('instructors/', views.instructor_list, name='instructor_list'),
     path('book-lesson/', views.book_lesson, name='book_lesson'),
     path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='user_login'),
 ]
