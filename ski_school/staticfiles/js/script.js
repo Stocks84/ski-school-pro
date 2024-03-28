c7cb431492baf68eae5c9322b3b19a9b0aec450c
William Stocks <willstocks@me.com>
2024-03-28 20:55:22 +0000

    Update Procfile

---
+++ ski_school/staticfiles/js/script.js
@@ -0,0 +1,10 @@
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code here
+    console.log('Your JavaScript file is loaded!');
+});
+
+function confirmDelete() {
+    console.log("Confirm delete function called");
+    return confirm("Are you sure you want to delete your bio?");
+}
