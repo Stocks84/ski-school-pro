da5253430fef4f504c29b6567d0c77f760b22bcb
William Stocks <willstocks@me.com>
2024-03-29 12:57:27 +0000

    Debug

--- ski_school/settings.py
+++ ski_school/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-gghbi&-94mmg(6l+6qn6!(@rppc1_458m%e&s79(x&_8vr4s_+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-stocks84-skischoolpro-u5399bv0eor.ws-eu110.gitpod.io',
 'ski-school-pro-2-4db020aae0bc.herokuapp.com']
